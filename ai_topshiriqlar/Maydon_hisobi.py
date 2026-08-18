55d6d005ef5060ebb6346f232967cf229fbc0db5
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 02:40:36 +1000

    Add AI task: Maydon hisobi

---
+++ ai_topshiriqlar/Maydon_hisobi.py
@@ -0,0 +1,18 @@
+# Maydon hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: O'rnatish va muhit — Python, interpreter, IDE sozlash
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+
+rem_area = (A * B) - (C * D)
+perim = 2 * (A + B)
+
+
+print(rem_area)
+print(perim)
