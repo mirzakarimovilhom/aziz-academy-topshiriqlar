2aab0ce899d1953f8111ba0514e4985d2e107c1c
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 02:34:36 +1000

    Add AI task: O'lchovlarni taqsimlash

---
+++ ai_topshiriqlar/Olchovlarni_taqsimlash.py
@@ -0,0 +1,11 @@
+# O'lchovlarni taqsimlash
+# Kurs: Dasturlash / IT
+# Mavzu: O'rnatish va muhit — Python, interpreter, IDE sozlash
+# Ball: 30
+# Aziz Academy — AI Topshiriq
+
+N = int(input())
+K = int(input())
+
+
+print(N // K)
