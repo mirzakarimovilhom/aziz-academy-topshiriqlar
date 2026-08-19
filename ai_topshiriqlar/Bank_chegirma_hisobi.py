5d59084363f97f1a6d259f225bb5aae3cf70e8d2
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 23:50:06 +1000

    Add AI task: Bank: chegirma hisobi

---
+++ ai_topshiriqlar/Bank_chegirma_hisobi.py
@@ -0,0 +1,18 @@
+# Bank: chegirma hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+narx1 = int(input())
+narx2 = int(input())
+
+
+jami = (narx1 * 0.7) + (narx2 * 0.7)
+
+
+if jami < 100000:
+    jami += 10000
+    
+    
+print(int(round(jami)))
