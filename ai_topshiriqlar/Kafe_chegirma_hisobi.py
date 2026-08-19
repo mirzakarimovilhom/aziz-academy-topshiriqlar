7da6c4e381415cd868480c3e480a1ba9a5d9d38f
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 12:37:18 +1000

    Add AI task: Kafe: chegirma hisobi

---
+++ ai_topshiriqlar/Kafe_chegirma_hisobi.py
@@ -0,0 +1,18 @@
+# Kafe: chegirma hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: O'rnatish va muhit — Python, interpreter, IDE sozlash
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+
+
+summa = (a * 0.75) + (b * 0.75)
+
+
+if summa < 100000:
+    summa += 10000
+    
+    
+print(int(summa))
