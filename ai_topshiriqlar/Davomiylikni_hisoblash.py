535e96314bb48dbc2f3eb4d64c5c6e548903e6f5
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 12:29:41 +1000

    Add AI task: Davomiylikni hisoblash

---
+++ ai_topshiriqlar/Davomiylikni_hisoblash.py
@@ -0,0 +1,25 @@
+# Davomiylikni hisoblash
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+h1 = int(input())
+m1 = int(input())
+h2 = int(input())
+m2 = int(input())
+
+
+start_minutes = h1 * 60 + m1 
+end_minutes = h2 * 60 + m2 
+
+
+diff_minutes = end_minutes - start_minutes
+
+
+hours = diff_minutes // 60
+minutes = diff_minutes % 60 
+
+
+print(hours)
+print(minutes)
