fd0906e6bb0a07fbc8129d066a045e9ca37e4f43
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 19:43:25 +1000

    Add AI task: Avtobus parki: chegirma hisobi

---
+++ ai_topshiriqlar/Avtobus_parki_chegirma_hisobi.py
@@ -0,0 +1,18 @@
+# Avtobus parki: chegirma hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+narx1 = int(input())
+narx2 = int(input())
+
+
+umumiy = (narx1 + narx2) * 0.85
+
+
+if umumiy < 100000:
+    umumiy += 10000
+    
+    
+print(int(umumiy))
