7d7773769e6d2880a6046e5ba9171eaac04fe159
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 18:05:23 +1000

    Add AI task: O'quvchilarni taqsimlash

---
+++ ai_topshiriqlar/Oquvchilarni_taqsimlash.py
@@ -0,0 +1,23 @@
+# O'quvchilarni taqsimlash
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+N = int(input())
+K = int(input())
+
+
+ulush = N // K 
+qoldiq = N % K
+
+
+if qoldiq == 0:
+    yetishmayotgan = 0
+else:
+    yetishmayotgan = K - qoldiq
+    
+    
+print(ulush)
+print(qoldiq)
+print(yetishmayotgan)
