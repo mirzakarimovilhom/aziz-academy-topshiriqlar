676947660bbe95d9f83e996dc205cbaa7292ab05
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-19 23:29:12 +1000

    Add AI task: Vaqtni birliklarga ajratish

--- ai_topshiriqlar/Vaqtni_birliklarga_ajratish.py
+++ ai_topshiriqlar/Vaqtni_birliklarga_ajratish.py
@@ -1,11 +1,25 @@
 # Vaqtni birliklarga ajratish
 # Kurs: Dasturlash / IT
 # Mavzu: Birinchi dastur ⭐ — print() va kommentlar
-# Ball: 30
+# Ball: 100
 # Aziz Academy — AI Topshiriq
 
-t = int(input())
-hours = t // 60 
-minutes = t % 60 
-print(hours)
-print(minutes)
+T = int(input())
+
+
+kun = T // 86400 
+T %= 86400 
+
+
+soat = T // 3600 
+T %= 3600 
+
+
+daqiqa = T // 60 
+sekund = T % 60 
+
+
+print(kun)
+print(soat)
+print(daqiqa)
+print(sekund)
