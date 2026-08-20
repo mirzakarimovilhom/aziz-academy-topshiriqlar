13c71fe6949ea2224e1ef51bd6959ff8c9f7da01
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-20 20:31:36 +1000

    Add AI task: Shaharlarni taqsimlash

---
+++ ai_topshiriqlar/Shaharlarni_taqsimlash.py
@@ -0,0 +1,16 @@
+# Shaharlarni taqsimlash
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+n = int(input())
+k = int(input())
+
+
+share = n // k
+rem = n % k
+need = 0 if rem == 0 else k - rem 
+
+
+print(share, rem, need, sep='\n')
