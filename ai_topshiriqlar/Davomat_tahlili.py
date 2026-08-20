af4c196e99c3843460c5acca4d7637e6a2201f05
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-20 20:27:23 +1000

    Add AI task: Davomat tahlili

--- ai_topshiriqlar/Davomat_tahlili.py
+++ ai_topshiriqlar/Davomat_tahlili.py
@@ -1,30 +1,10 @@
 # Davomat tahlili
 # Kurs: Dasturlash / IT
-# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Mavzu: Sonlar: int va float — butun va kasr sonlar
 # Ball: 100
 # Aziz Academy — AI Topshiriq
 
 n = int(input())
-days = list(map(int, input().split()))
-
-
-total_present = sum(days)
-
-
-max_streak = 0
-current_streak = 0
-for day in days:
-    if day == 1:
-        current_streak += 1
-        if current_streak > max_streak:
-            max_streak = current_streak
-    else:
-        current_streak = 0 
-        
-        
-percentage = (total_present* 100) // n 
-
-
-print(total_present)
-print(max_streak)
-print(percentage)
+d = input().split()
+c = d.count('1')
+print(c, max(map(len, "".join(d).split('0'))), c * 100 // n, sep='\n')
