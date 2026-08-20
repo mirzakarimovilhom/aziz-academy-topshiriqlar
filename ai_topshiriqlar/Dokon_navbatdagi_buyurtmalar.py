4c4416ba2eeefb12ad31480b6797b43a777feed6
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-20 21:18:46 +1000

    Add AI task: Do'kon: navbatdagi buyurtmalar

--- ai_topshiriqlar/Dokon_navbatdagi_buyurtmalar.py
+++ ai_topshiriqlar/Dokon_navbatdagi_buyurtmalar.py
@@ -1,16 +1,16 @@
 # Do'kon: navbatdagi buyurtmalar
 # Kurs: Dasturlash / IT
-# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Mavzu: Sonlar: int va float — butun va kasr sonlar
 # Ball: 100
 # Aziz Academy — AI Topshiriq
 
-n = int(input())
-s, c = 0, 0 
-for i in range(n):
-    p = int(input())
-    if s + p <= 50000:
-        s += p 
-        c += 1 
+data = [*map(int, open(0).read().split())]
+n = data[0]
+s = c = 0
+for x in data[1:]:
+    if s + x <= 100000:
+        s += x 
+        c += 1
     else:
         break
 print(c, s, n - c, sep="\n")
