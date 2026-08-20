f631031b1091351f68c0296262a3473d76811dbc
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-08-20 20:46:37 +1000

    Add AI task: O'rtacha bahosi

---
+++ ai_topshiriqlar/Ortacha_bahosi.py
@@ -0,0 +1,8 @@
+# O'rtacha bahosi
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+l = sorted([int(input()) for _ in range(5)])
+print(sum(l[1:4]) // 3)
