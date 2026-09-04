d36748e67211156be027a179d0cdd55cb47b2be4
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-09-04 14:39:28 +1000

    Add AI task: Mashg'ulotlarni taqsimlash

---
+++ ai_topshiriqlar/Mashgulotlarni_taqsimlash.py
@@ -0,0 +1,23 @@
+# Mashg'ulotlarni taqsimlash
+# Kurs: Dasturlash / IT
+# Mavzu: Dasturlashga kirish — Python nima va nega o'rganamiz
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+N = int(input())
+K = int(input())
+
+
+ulush = N // K
+qoldiq = N % K 
+
+
+if qoldiq == 0:
+    yetishmayotgan = 0 
+else:
+    yetishmayotgan = K - qoldiq 
+    
+    
+print(ulush)
+print(qoldiq)
+print(yetishmayotgan)
