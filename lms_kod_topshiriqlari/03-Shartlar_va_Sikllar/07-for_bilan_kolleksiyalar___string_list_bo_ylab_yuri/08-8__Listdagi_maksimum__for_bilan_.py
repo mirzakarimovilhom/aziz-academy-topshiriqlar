95b863af7571097129b472a50f8d2a73c64a61b0
mirzakarimovilhom <mirzakarimovilhom285@gmail.com>
2026-09-14 19:30:24 +1000

    Add: 8) Listdagi maksimum (for bilan) [mirzakarimovilhom]

---
+++ lms_kod_topshiriqlari/03-Shartlar_va_Sikllar/07-for_bilan_kolleksiyalar___string_list_bo_ylab_yuri/08-8__Listdagi_maksimum__for_bilan_.py
@@ -0,0 +1,11 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+
+max_num = numbers[0]
+for num in numbers:
+    if num > max_num:
+        max_num = num 
+        
+        
+print(max_num)
